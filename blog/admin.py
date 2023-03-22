8d787f0551620733ae365083b81dcd79c7509114
alita <alita.avs@gmail.com>
2023-03-22 23:35:54 +0200

    fixture, management

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from blog.models import Comment, Post
+
 # Register your models here.
+# admin.site.register(Post)
+# admin.site.register(Comment)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'text', 'post_date')
+
+    search_fields = ['title', 'text']
+    date_hierarchy = 'post_date'
+    list_per_page = 10
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'owner', 'text')
+
+    list_per_page = 20
--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,14 @@ class Post(models.Model):
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
     post_date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     owner = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
     text = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.text
